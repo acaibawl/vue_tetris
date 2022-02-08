0cf608666778515b5fddac35a98f3e22eb17502e
acaiibawl <hagiya0719@gmail.com>
2022-02-08 12:18:50 +0900

    トップ画面と、画面遷移を実装

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { RouteRecordRaw, createWebHistory, createRouter } from 'vue-router';
 
-createApp(App).mount('#app')
+import TopPage from './pages/TopPage.vue';
+import PlayPage from './pages/PlayPage.vue';
+
+// 各画面のルーティング情報
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'TopPage',
+    component: TopPage,
+  },
+  {
+    path: '/play',
+    name: 'PlayPage',
+    component: PlayPage
+  },
+];
+
+// 各画面のルーティング情報からルーターを作成
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+});
+
+createApp(App).use(router).mount('#app')
