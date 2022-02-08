c28b3cdeb1e2784fc881a8d869edb1fafd8f752f
acaiibawl <hagiya0719@gmail.com>
2022-02-08 18:08:59 +0900

    ↑キーで即時落下

--- src/pages/PlayPage.vue
+++ src/pages/PlayPage.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-  import { reactive } from 'vue';
+  import { reactive, onMounted, onBeforeUnmount } from 'vue';
   import { Tetromino, TETROMINO_TYPE } from '../common/Tetromino';
   import { Field } from '../common/Field';
   // VSCodeのVeturプラグインで未定義・使われていないのエラーが出るが、プラグインが<script setup>に未対応なだけ
@@ -58,17 +58,56 @@ import TetrominoPreviewComponent from '../components/TetrominoPreviewComponent.v
     tetromino.position = { x: 3, y: 0 };
   };
 
- setInterval(() => {
-   tetris.field = Field.deepCopy(staticField);
+  const onKeyDown = (e: KeyboardEvent) => {
+    switch (e.key) {
+      case "Down":
+      case "ArrwDown":
+        if (canDropCurrentTetromino()) {
+          tetromino.position.y++;
+          resetDrop();
+        } else {
+          nextTetrisField();
+        }
+        break;
+      case "Up":
+      case "ArrowUp":
+        while(canDropCurrentTetromino()) {
+          tetromino.position.y++;
+          resetDrop();
+        }
+        nextTetrisField();
+        break;
+    }
+  }
 
-   if (canDropCurrentTetromino()) {
-      tetromino.position.y++;
-   } else {
-     nextTetrisField();
-   }
- 
- }, 1 * 100);
- tetris.field.update(tetromino.current.data, tetromino.position);
+  onMounted(function() {
+    document.addEventListener('keydown', onKeyDown);
+  });
+  onBeforeUnmount(function() {
+    document.removeEventListener('keydown', onKeyDown);
+  });
+
+  const resetDropInterval = () => {
+    let intervalId = -1;
+
+    return () => {
+      if (intervalId !== -1) clearInterval(intervalId);
+
+      intervalId = setInterval(() => {
+        tetris.field = Field.deepCopy(staticField);
+
+          if (canDropCurrentTetromino()) {
+              tetromino.position.y++;
+          } else {
+            nextTetrisField();
+          }
+        }, 1 * 1000);
+    };
+  };
+
+  const resetDrop = resetDropInterval();
+
+  resetDrop();
 </script>
 
 <template>
